d612ba87c0d023a4e3bbb2e5fb2c2c50272524c4
Jordan Johnson <Jordan Johnson>
2017-04-10 01:33:46 -0400

    First Commit

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,23 @@
+﻿using System.Web.Mvc;
+using Vidly.Models;
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek!"};
+            //return Content("Hello World");
+
+            //return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name" });
+            return View(movie);
+        }
+        [Route("movies/released/{year}/{month:regex(\\d{4}): range(1,12)}")]
+        public ActionResult ByReleaseDate(int year, int month)
+
+        {
+            return Content(year +"/" + month);
+        }
+    }
+}
